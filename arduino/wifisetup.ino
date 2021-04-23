3b4f90a7a13fb76f5dfd9f88b9adcc19b75a6841
Nupsi <nupsi4.thenoob@gmail.com>
2021-04-23 16:59:24 +0300

    Removed node_modules folder. Added .gitignore. Created docker-compose for backend, it now uses mongoose to store data. Updated api to support multiple devices based on a id. Updated frontend to display line chart of the selected device temperature

---
+++ arduino/wifisetup.ino
@@ -0,0 +1,74 @@
+#include <SPI.h>
+#include <WiFi101.h>
+#include <WifiClient.h>
+#include <TH02_dev.h>
+#include "Arduino.h"
+#include "Wire.h"
+
+/**
+ * Wifi settings.
+ */
+IPAddress server(192,168,10,49);
+int port = 3000;
+char ssid[] = "";       
+char pass[] = "";  
+int status = WL_IDLE_STATUS;     
+
+void setup() {
+
+  Serial.begin(9600);
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(5000);
+  printCurrentNet();
+  sendSensorData();
+}
+
+/**
+ * Print out connection information.
+ */
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+}
+
+/**
+ * Send out the current sensor data.
+ */
+void sendSensorData() {
+  WiFiClient client;
+  String data = "{\"temperature\": 45, \"isFull\": true}";
+  if(!client.connected()){
+    if(client.connect(server, port)) {
+      Serial.println("Connected");
+      // Write a HTTP POST request. 
+      client.println("POST /palju HTTP/1.1");
+      client.println("User-Agent: Arduino/1.0");
+      client.println("Connection: close");
+      client.println("Content-Type: application/json");
+      client.print("Content-Length: ");
+      client.println(data.length());
+      client.println(); 
+      client.println(data);
+    }
+  }
+  client.stop();
+}
+
+
--- arduino_secrets.h
+++
@@ -1,2 +0,0 @@
-#define SECRET_SSID "Ylakerta"
-#define SECRET_PASS "nuppuverkko"
